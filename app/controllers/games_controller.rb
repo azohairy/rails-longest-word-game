9596fbdc1cadb5c24c24e192ff5fe885aed671ef
Abdelrahman Elzohairy <a.zohairy@gmail.com>
2019-11-04 17:22:59 +0100

    Add score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,19 +4,20 @@ require 'json'
 class GamesController < ApplicationController
   def new
     @letters = (0...10).map { ('A'..'Z').to_a.sample }
+    @start_time = Time.now
   end
 
   def score
-    @answer = params[:answer]
-    @generated = params[:letters]
-    url = "http://wagon-dictionary.herokuapp.com/#{@answer}"
-    word_serialized = open(url).read
-    word = JSON.parse(word_serialized)
-    if word['found']
+    @answer = params[:answer].upcase
+    @generated = params[:letters].upcase
+    @duration = Time.now - Time.parse(params[:time])
+    @score = ((@answer.length / @duration) * 100).to_i
+    if api(@answer)['found']
       checker(@answer, @generated) ? @result = "Congratulations! #{@answer} is a valid English word!" : @result = "Sorry but #{@answer} can't be built out of #{@generated}!"
     else
       @result = "Sorry but #{@answer} doesn't seem to be a valid English word.."
     end
+    raise
   end
 
   def checker(word, grid)
@@ -26,6 +27,13 @@ class GamesController < ApplicationController
       word_array.count(letter) <= grid.count(letter)
     end
     charac_check && try
+  end
+
+  def api(answer)
+    url = "http://wagon-dictionary.herokuapp.com/#{answer}"
+    word_serialized = open(url).read
+    word = JSON.parse(word_serialized)
+    word
   end
 end
 
